a026bebe72cfb55f5e611a8d7833ff0442be92df
Waleeja Ali <waleeja.ali25@gmail.com>
2026-08-12 00:07:43 +0500

    fix: fix exception

--- src/common/filters/database-exception.filter.ts
+++ src/common/filters/database-exception.filter.ts
@@ -3,6 +3,7 @@ import { QueryFailedError } from 'typeorm';
 import { RpcException } from '@nestjs/microservices';
 import { MYSQL_ERRORS } from '../constants';
 import { status as GrpcStatus } from '@grpc/grpc-js';
+import { throwError } from 'rxjs';
 
 @Catch(QueryFailedError)
 export class DatabaseExceptionFilter implements ExceptionFilter<QueryFailedError> {
@@ -14,27 +15,40 @@ export class DatabaseExceptionFilter implements ExceptionFilter<QueryFailedError
 
     switch (driverError.errno) {
       case MYSQL_ERRORS.DUPLICATE_ENTRY:
-        throw new RpcException({
-          code: GrpcStatus.ALREADY_EXISTS,
-          message: 'Duplicate value already exists',
-        });
+        return throwError(
+          () =>
+            new RpcException({
+              code: GrpcStatus.ALREADY_EXISTS,
+              message: 'Duplicate value already exists',
+            }),
+        );
 
       case MYSQL_ERRORS.ROW_IS_REFERENCED:
-        throw new RpcException({
-          code: GrpcStatus.FAILED_PRECONDITION,
-          message: 'Record is still referenced by other records',
-        });
+        return throwError(
+          () =>
+            new RpcException({
+              code: GrpcStatus.FAILED_PRECONDITION,
+              message: 'Record is still referenced by other records',
+            }),
+        );
 
       case MYSQL_ERRORS.NO_REFERENCED_ROW:
-        throw new RpcException({
-          code: GrpcStatus.INVALID_ARGUMENT,
-          message: 'Referenced record does not exist',
-        });
+        return throwError(
+          () =>
+            new RpcException({
+              code: GrpcStatus.INVALID_ARGUMENT,
+              message: 'Referenced record does not exist',
+            }),
+        );
+
       default:
-        throw new RpcException({
-          code: GrpcStatus.INTERNAL,
-          message: 'Internal Server Error',
-        });
+        return throwError(
+          () =>
+            new RpcException({
+              code: GrpcStatus.INTERNAL,
+              message: 'Internal Server Error',
+            }),
+        );
     }
   }
 }
